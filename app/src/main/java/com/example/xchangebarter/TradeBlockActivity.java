5afb035cdef3ac2673f86667e8b3225ce209d2a3
Gib Barbo <fourthgib@gmail.com>
2023-05-11 16:41:31 -0700

    - Added views and layouts to Trade Block

--- app/src/main/java/com/example/xchangebarter/TradeBlockActivity.java
+++ app/src/main/java/com/example/xchangebarter/TradeBlockActivity.java
@@ -7,17 +7,26 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class TradeBlockActivity extends AppCompatActivity {
 
     private Button back;
+    private ImageView tb_home, tb_tradeblock, tb_inventory, tb_profile;
 
-    private ImageView notif_home, notif_bell, notif_inventory, notif_profile;
+    private RecyclerView active_rv;
+    private RecyclerView pending_rv;
 
+    private String user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trade_block);
+        // get user email for filter and to pass to other activities
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            user = extras.getString("user");
+        }
         back = (Button) findViewById(R.id.notif_back);
 
         back.setOnClickListener(v -> {
@@ -28,38 +37,41 @@ public class TradeBlockActivity extends AppCompatActivity {
         });
         init();
 
-        notif_home.setOnClickListener(v -> {
+        tb_home.setOnClickListener(v -> {
             Toast.makeText(TradeBlockActivity.this, "Home Click", Toast.LENGTH_SHORT).show();
             Intent homeIntent = new Intent(TradeBlockActivity.this, Home2Activity.class);
+            homeIntent.putExtra("user", user);
             startActivity(homeIntent);
         });
 
-        notif_bell.setOnClickListener(v -> {
-            Toast.makeText(TradeBlockActivity.this, "Bell Click", Toast.LENGTH_SHORT).show();
-            Intent bellIntent = new Intent(TradeBlockActivity.this, TradeBlockActivity.class);
-            startActivity(bellIntent);
-            finish();
+        tb_tradeblock.setOnClickListener(v -> {
+            Toast.makeText(TradeBlockActivity.this, "You are on Trade Block", Toast.LENGTH_SHORT).show();
+            /*Intent tbIntent = new Intent(TradeBlockActivity.this, TradeBlockActivity.class);
+            startActivity(tbIntent);
+            finish();*/
         });
 
-        notif_inventory.setOnClickListener(v -> {
+        tb_inventory.setOnClickListener(v -> {
             Toast.makeText(TradeBlockActivity.this, "Inventory Click", Toast.LENGTH_SHORT).show();
             Intent invIntent = new Intent(TradeBlockActivity.this, InventoryActivity.class);
             startActivity(invIntent);
+            invIntent.putExtra("user", user);
             finish();
         });
 
-        notif_profile.setOnClickListener(v -> {
+        tb_profile.setOnClickListener(v -> {
             Toast.makeText(TradeBlockActivity.this, "Profile Click", Toast.LENGTH_SHORT).show();
             Intent profileIntent = new Intent(TradeBlockActivity.this, ProfileActivity.class);
             startActivity(profileIntent);
+            profileIntent.putExtra("user", user);
             finish();
         });
     }
 
     private void init(){
-        notif_home = findViewById(R.id.notif_home_btn);
-        notif_bell = findViewById(R.id.notif_tradeblock_btn);
-        notif_inventory = findViewById(R.id.notif_inventory_btn);
-        notif_profile = findViewById(R.id.notif_profile_btn);
+        tb_home = findViewById(R.id.tb_home_btn);
+        tb_tradeblock = findViewById(R.id.tb_tradeblock_btn);
+        tb_inventory = findViewById(R.id.tb_inventory_btn);
+        tb_profile = findViewById(R.id.tb_profile_btn);
     }
 }
